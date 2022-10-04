3e20811ca771099403de6e4302334bebb992c155
SergeyPichugin <8490839@mail.ru>
2022-10-04 16:07:13 +0300

    fix ProductList 2

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -66,7 +66,7 @@ const ProductList = () => {
             },
             body: JSON.stringify(data)
         })
-    }, [addedItems])
+    }, [addedItems, queryId])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
