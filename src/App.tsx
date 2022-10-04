0bf5a5621c7e34f3d95d5a6b490554c56fcde052
SergeyPichugin <8490839@mail.ru>
2022-10-04 13:20:16 +0300

    update From

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React, {useEffect} from 'react';
 import './App.css';
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header";
+import {Route, Routes} from "react-router-dom"
+import ProductList from "./components/ProductList";
+import Form from './components/Form';
 
 function App() {
 
@@ -12,10 +15,13 @@ function App() {
     }, [])
 
   return (
-    <div className="App">
-        <Header/>
-        <button onClick={onToogleButton}>toogle</button>
-    </div>
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route index element={<ProductList/>}/>
+                <Route path={'form'} element={<Form/>}/>
+            </Routes>
+        </div>
   );
 }
 
